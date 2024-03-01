c990f1927813e9ab1097cb3eee95d2015e3fdedb
Guto <gutodidonato@gmail.com>
2024-03-01 12:29:47 -0300

    implementado primeiro metodo

--- main.py
+++ main.py
@@ -13,21 +13,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 fake = Faker()
-termos_nomes = [fake.name() for _ in range(1000)]
-
-
-def fuxicar_captcha(navegador):
-    try:
-        pagina_html = navegador.page_source
-        print(pagina_html)
-        pagina = WebDriverWait(navegador, 10).until(
-            EC.url_contains("www.google.com/sorry/index?continue")
-        )
-        autogui_clicker.realizar_sequencia_de_cliques()
-        print("encontrado!")
-    except:
-        print("não encontrado")
-        pass
+termos_nomes = [fake.name() for _ in range(2000)]
 
 
 lista_sites = []
@@ -52,7 +38,7 @@ try:
     for termo in termos_nomes:
         navegador.get("https://www.google.com")
         try:
-            fuxicar_captcha(navegador)
+            print("bloco de aceitar cookies")
             botao_aceitar_tudo = WebDriverWait(navegador, 5).until(
                 EC.presence_of_element_located(
                     (By.XPATH, "//div[text()='Aceitar tudo']")
@@ -62,16 +48,21 @@ try:
             if botao_aceitar_tudo.is_displayed():
                 botao_aceitar_tudo.click()
         except:
-            fuxicar_captcha(navegador)
-            campo_pesquisa = WebDriverWait(navegador, 10).until(
+            campo_pesquisa = WebDriverWait(navegador, 5).until(
                 EC.presence_of_element_located((By.NAME, "q"))
             )
             campo_pesquisa.send_keys(termo + " " + valor_fixo)
             campo_pesquisa.send_keys(Keys.RETURN)
+            time.sleep(random.choice(range(5, 10)))
+            try:
+                WebDriverWait(navegador, 5).until(
+                    EC.presence_of_element_located((By.ID, "result-stats"))
+                )
+                lista_sites.append(navegador.current_url)
+            except:
+                print("bloco de auto")
+                autogui_clicker.realizar_sequencia_de_cliques()
 
-            time.sleep(random.choice(range(5, 20)))
-
-            lista_sites.append(navegador.current_url)
 
 except Exception as e:
     print(f"Erro durante a execução: {e}")
